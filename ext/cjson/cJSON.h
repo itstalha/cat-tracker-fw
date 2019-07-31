1e5a5ed95903fdda7df7ebe5f26d767ce7d22710
Røstad <sir1@ssr.nordicsemi.no>
2019-07-31 11:29:20 +0200

    samples: nrf9160: cat_tracker: Bugfixing in cjson parser, clean up main loop

--- ext/cjson/cJSON.h
+++ ext/cjson/cJSON.h
@@ -111,9 +111,9 @@ extern cJSON *cJSON_CreateArray(void);
 extern cJSON *cJSON_CreateObject(void);
 
 /* These utilities create an Array of count items. */
-extern cJSON *cJSON_CreateIntArray(int *numbers, int count);
+extern cJSON *cJSON_CreateIntArray(const int *numbers, int count);
 extern cJSON *cJSON_CreateFloatArray(const float *numbers, int count);
-extern cJSON *cJSON_CreateDoubleArray(double *numbers, int count);
+extern cJSON *cJSON_CreateDoubleArray(const double *numbers, int count);
 extern cJSON *cJSON_CreateStringArray(const char **strings, int count);
 
 /* Append item to the specified array/object. */
